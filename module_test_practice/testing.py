433c1026e0729341b87ee3e803d9c993086481ec
Joseph <joey.callender1@outlook.com>
2025-10-02 15:32:49 -0500

    adding testing modules

---
+++ module_test_practice/testing.py
@@ -0,0 +1,19 @@
+# Basic python module for demonstration purposes
+
+def main():
+
+    def add(a, b):
+        return a + b
+
+    def multiply(a, b):
+        return a * b
+
+    def subtract(a, b):
+        return a - b
+
+    def divide(a, b):
+        return a / b
+
+
+if __name__ == "__main__":
+    main()
