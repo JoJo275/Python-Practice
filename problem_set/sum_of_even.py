cd8d8f85e704e93cb0f0740030d852a5b7434a71
Joseph <joseph26584@gmail.com>
2025-10-17 12:00:56 -0500

    first problem file set up for problem_set dir

---
+++ problem_set/sum_of_even.py
@@ -0,0 +1,21 @@
+"""sum_of_even.py
+
+1) sum_of_even(n)
+- Description: Return the sum of all even numbers from 1 to n (inclusive). If
+n < 1 return 0.
+- Input: integer n >= any int
+- Output: integer
+
+"""
+
+
+def main():
+    """Main function to demonstrate sum_of_even usage."""
+
+    def sum_of_even(n):
+        """Return the sum of all even numbers from 1 to n (inclusive). If n < 1 return 0."""
+        
+
+
+if __name__ == "__main__":
+    main()
