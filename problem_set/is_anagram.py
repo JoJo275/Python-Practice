4f03c572a33263a85744e86c83e488a8978d8cfa
Joseph <joseph26584@gmail.com>
2025-10-18 15:21:37 -0500

    initialized is_anagram.py file

---
+++ problem_set/is_anagram.py
@@ -0,0 +1,18 @@
+"""is_anagram.py
+
+2) is_anagram(s1, s2)
+- Description: Return True if `s1` and `s2` are anagrams (case-insensitive, ignore spaces and punctuation), otherwise False.
+- Input: two strings
+- Output: boolean
+
+"""
+
+#!/usr/bin/env python3
+
+def main():
+
+    def is_anagram(s1, s2):
+
+
+if __name__ == "__main__":
+    main()
