7984105c735a93df447bfb38a483a1ec8f59c5d8
Joseph <joseph26584@gmail.com>
2025-10-18 15:26:50 -0500

    first iteration of is_anagram.py

--- problem_set/is_anagram.py
+++ problem_set/is_anagram.py
@@ -7,11 +7,25 @@
 
 """
 
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+
 
 def main():
 
     def is_anagram(s1, s2):
+        """
+        Docstring for is_anagram
+
+        :param s1: Description
+        :param s2: Description
+        """
+        def normalize(s):
+            s = s.lower()
+            return ''.join(ch for ch in s if ch.isalnum())
+
+        a = normalize(s1)
+        b = normalize(s2)
+        return sorted(a) == sorted(b)
 
 
 if __name__ == "__main__":
