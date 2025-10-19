08ea59bde017ab8b2220031890a1cb2e8986538b
Joseph <joseph26584@gmail.com>
2025-10-19 17:53:07 -0500

    initialized fibonacci.py file

---
+++ problem_set/fibonacci.py
@@ -0,0 +1,20 @@
+""" fibonacci(n)
+
+3) fibonacci(n)
+- Description: Return the nth Fibonacci number, with fibonacci(0)=0, fibonacci(1)=1. For n<0 raise ValueError.
+- Input: integer n
+- Output: integer
+
+"""
+
+# !/usr/bin/env python3
+
+
+
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
