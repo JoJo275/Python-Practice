1f7ee95544e35dccca9d89f389abc52388df1683
Joseph <joseph26584@gmail.com>
2025-10-20 09:57:55 -0500

    initialized flatten.py file

---
+++ problem_set/flatten.py
@@ -0,0 +1,11 @@
+"""flatten.py
+
+4) flatten(lst)
+- Description: Given a list which can contain nested lists to arbitrary depth, return a flattened list with all elements in order.
+- Input: list (elements can be lists)
+- Output: list
+
+"""
+
+#!/usr/bin/env python3
+
