9acd977e4086adaf2222cfbd2c3c2ad1eefc5d5e
Joseph <joseph26584@gmail.com>
2025-10-21 09:38:47 -0500

    added group_by_key file initialized

---
+++ problem_set/group_by_key.py
@@ -0,0 +1,11 @@
+"""group_by_key.py
+
+5) group_by_key(items, key)
+- Description: Given a list of dicts `items` and a `key` string, return a dict
+mapping key values to lists of items with that key. If an item doesn't have
+the key, group under None.
+
+- Input: list of dicts, string key
+- Output: dict
+
+"""
