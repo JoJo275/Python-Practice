1f63df9c279d82f5308b8bb03512864ffa26efb7
Joseph <joseph26584@gmail.com>
2025-10-23 09:04:36 -0500

    intialized shortest_path.py

---
+++ problem_set/shortest_path.py
@@ -0,0 +1,25 @@
+"""
+
+6) shortest_path(grid)
+- Description: Given a 2D grid (list of lists) with 0 for open cell and 1 for
+wall, return the length of shortest path from top-left (0,0) to bottom-right
+(m-1,n-1) moving 4-directionally. If no path, return -1. Grid is non-empty.
+- Input: list[list[int]]
+- Output: integer
+
+"""
+
+# !/usr/bin/env python3
+
+
+
+
+
+
+
+def main() -> None:
+
+
+
+if __name__ == "__main__":
+    main()
