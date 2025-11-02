70d67b525f28ba706faef10973e6ed81b0a1e8d6
Joseph <joseph26584@gmail.com>
2025-11-01 21:28:02 -0500

    even_or_odd.py 1.0

--- problem_set1.0/even_or_odd.py
+++ problem_set1.0/even_or_odd.py
@@ -4,3 +4,23 @@
 Check if a number is even or odd.
 
 """
+
+def is_even_or_odd(number):
+    """Determine if a number is even or odd.
+
+    Args:
+        number (int): The number to check.
+
+    Returns:
+        str: "Even" if the number is even, "Odd" if the number is odd.
+    """
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+if __name__ == "__main__":
+    num = int(input("Enter an integer: "))
+    result = is_even_or_odd(num)
+    print(f"The number {num} is {result}.")
