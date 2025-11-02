73bdfb631bb46b7d7bdcd901f026320f895ba859
Joseph <joseph26584@gmail.com>
2025-11-02 17:52:26 -0600

    Initialized age_title.py.

---
+++ problem_set1.0/age_title.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+"""
+
+Ask the user for their age and print whether they are a minor, adult, or senior.
+
+"""
+
+
+def get_age_title(age):
+    if age < 18:
+        return "minor"
+    elif age < 65:
+        return "adult"
+    else:
+        return "senior"
+
+if __name__ == "__main__":
+    age = int(input("Enter your age: "))
+    title = get_age_title(age)
+    print(f"You are classified as a {title}.")
