8d0760eba1fa13100e964a481a362bc2ee2a6503
Joseph <joseph26584@gmail.com>
2025-11-08 19:17:19 -0600

    positive_negative.py 1.0.

---
+++ problem_set1.0/positive_negative.py
@@ -0,0 +1,18 @@
+"""
+
+Check whether a number is positive, negative, or zero.
+
+"""
+
+def check_number(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "Zero"
+    
+if __name__ == "__main__":
+    number = float(input("Enter a number: "))
+    result = check_number(number)
+    print(f"The number is {result}.")
