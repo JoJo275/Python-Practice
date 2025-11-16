14023b7a5e94bc48d4fc9d79b63698f28e6ee1b9
Joseph <joseph26584@gmail.com>
2025-11-16 17:59:36 -0600

    Added have_common_element function in common_element.py.

--- problem_set1.0/common_element.py
+++ problem_set1.0/common_element.py
@@ -5,3 +5,19 @@ Check if two sets have any elements in common.
 
 """
 
+from typing import Set, Any
+
+
+def have_common_element(set1: Set[Any], set2: Set[Any]) -> bool:
+    """
+    Determine if two sets have at least one element in common.
+
+    This function uses set intersection to efficiently check for common elements.
+
+    Args:
+        set1: The first set of elements.
+        set2: The second set of elements.
+
+    Returns:
+        True if there is at least one common element, False otherwise.
+    """
