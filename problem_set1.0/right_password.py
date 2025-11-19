9b7ba20fec604c71aaf99fa5bb797390f207da7b
Joseph <joseph26584@gmail.com>
2025-11-18 19:15:39 -0600

    First part of docstring complete for right_password.py.

--- problem_set1.0/right_password.py
+++ problem_set1.0/right_password.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """right_password.py
 
-
+In a file named right_password.py, implement a program that checks whether a
+user-entered password matches a stored password.
 
 """
+
