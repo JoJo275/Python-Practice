5f75074799c09a3ca0af7d66ae1fe802087abe4e
Joseph <joseph26584@gmail.com>
2025-12-31 15:16:39 -0600

    Added documentation in the form of comments throughout 'simple_class.py' file.

--- class_practice/simple_class.py
+++ class_practice/simple_class.py
@@ -5,16 +5,20 @@ Simple example of a class in Python from CS50 - Lecture 8 - Object Oriented Prog
 
 """
 
-
+# Initializing class "Student".
 class Student:
     ...
 
 
+# Defining main function which carries student object initialization and
+# printing of attributes.
 def main():
     student = Student()
     print(f"{student.name} from {student.house}")
 
 
+# Function to get student details from user input using the "Student" class,
+# "student" object, and "student" attributes.
 def get_student():
     student = Student()
     student.name = input("Name: ")
