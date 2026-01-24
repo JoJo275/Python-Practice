beaa268a7b41444eae82bf7697aa23ef96d5eace
Joseph <joseph26584@gmail.com>
2026-01-24 15:33:06 -0600

    Add initial FastAPI framework example in 'e3.py'

---
+++ frameworks/e3.py
@@ -0,0 +1,8 @@
+"""
+
+A python framework example file.
+Demonstrates FastAPI - a modern, fast (high-performance) web framework for
+building APIs with Python 3.7+ based on standard Python type hints.
+
+"""
+
